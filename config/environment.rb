f2ddc3b2a24b52a765990e55cc137e832fe86908
john <john@klairvoyant.com>
2012-04-13 22:18:55 +0530

    April 13 2012

--- config/environment.rb
+++ config/environment.rb
@@ -1,8 +1,18 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
-
 # Initialize the rails application
 Klairvoyantsite::Application.initialize!
 
+config.action_mailer.delivery_method = :smtp
+ActionMailer::Base.smtp_settings= {
+    :enable_starttls_auto=> true,
+    :address	=>"smtp.gmail.com",
+    :port 		=>587,
+    :domain =>"klairvoyant.com",
+    :user_name=> "contact@klairvoyant.com",
+    :password =>"cochinkerala",
+    :authentication=>"plain"
+
+}
 
 
--- config/initializers/mailer.rb
+++ config/initializers/mailer.rb
@@ -1,10 +1,11 @@
-ActionMailer::Base.smtp_settings= {
-   :enable_starttls_auto=> true,
-   :address	=>"smtp.gmail.com",
-   :port 		=>587,
-   :domain =>"klairvoyant.com",
-   :user_name=> "contact@klairvoyant.com",
-   :password =>"cochinkerala",
-   :authentication=>"plain"
+#config.action_mailer.delivery_method = :smtp
+#ActionMailer::Base.smtp_settings= {
+ #  :enable_starttls_auto=> true,
+  # :address	=>"smtp.gmail.com",
+   #:port 		=>587,
+#   :domain =>"klairvoyant.com",
+ #  :user_name=> "contact@klairvoyant.com",
+ #  :password =>"cochinkerala",
+  # :authentication=>"plain"
 
-}
+#}
