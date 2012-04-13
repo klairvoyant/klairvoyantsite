87d2323d8e5316b96350e61674fb45d6dbd413d1
john <john@klairvoyant.com>
2012-04-13 23:48:58 +0530

    April 13 eve 2012

--- config/initializers/mailer.rb
+++ config/initializers/mailer.rb
@@ -1,9 +1,10 @@
-ActionMailer::Base.smtp_settings = {
-    :address        => 'smtp.sendgrid.net',
-    :port           => '587',
-    :authentication => :plain,
-    :user_name      => ENV['SENDGRID_USERNAME'],
-    :password       => ENV['SENDGRID_PASSWORD'],
-    :domain         => 'heroku.com'
+ActionMailer::Base.smtp_settings= {
+    :enable_starttls_auto=> true,
+    :address	=>"smtp.gmail.com",
+    :port 		=>587,
+    :domain =>"klairvoyant.com",
+    :user_name=> "contact@klairvoyant.com",
+    :password =>"cochinkerala",
+    :authentication=>"plain"
+
 }
-ActionMailer::Base.delivery_method = :smtp
