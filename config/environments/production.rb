dac7ae4d921b1d2746131c8f3ca508d663f4ae11
john <john@klairvoyant.com>
2012-04-14 23:20:16 +0530

    April 14  eve 2012

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -44,7 +44,7 @@ Klairvoyantsite::Application.configure do
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
-  config.assets.precompile += ['application.css']
+  config.assets.precompile += %w('application.css')
 
   # Disable delivery errors, bad email addresses will be ignored
   config.action_mailer.raise_delivery_errors = true
