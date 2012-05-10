1fc84ba0a9c5d1d164e0b51f2f1fdd4eb6703c24
john <john@klairvoyant.com>
2012-05-10 17:00:42 +0530

    may 10 with mobile and tablet code 2

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,5 +2,5 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
   has_mobile_fu
   #before_filter :force_mobile_format
-  before_filter :force_tablet_format
+  #before_filter :force_tablet_format
 end
